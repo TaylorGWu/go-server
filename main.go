e3f8fb89f34085530ae4ee9bd50792809675f483
Taylor <453014399@qq.com>
2020-05-01 16:02:07 +0800

    update func name

--- main.go
+++ main.go
@@ -12,12 +12,12 @@ func NewHandler(handler http.HandlerFunc) (http.HandlerFunc) {
 	})
 }
 
-func loginHandler(w http.ResponseWriter, r *http.Request) {
+func logicHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("start login")
 }
 
 func main() {
 	mutex := http.NewServeMux()
-	mutex.Handle("/api", NewHandler(loginHandler))
+	mutex.Handle("/api", NewHandler(logicHandler))
 	http.ListenAndServeTLS(":9920", "server.crt", "server.key", mutex)
 }
